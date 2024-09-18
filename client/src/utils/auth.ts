1225eb18c251c13c745889d2828177107f137d2a
unknown <rosa.angel.daniel@gmail.com>
2024-09-18 12:06:01 -0600

    added middleware auth

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,5 +1,4 @@
 import { JwtPayload, jwtDecode } from 'jwt-decode';
-// import 'dotenv/config';
 import { UserData } from '../interfaces/UserData';
 
 
@@ -15,7 +14,6 @@ class AuthService {
   }
   
   isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
     try {
       const decoded = jwtDecode<JwtPayload>(token);
       if (decoded?.exp && decoded.exp < Date.now() / 1000) {
@@ -27,7 +25,6 @@ class AuthService {
   }
 
   getToken(): string {
-    // TODO: return the token
     const loggedUser = localStorage.getItem('id_token') || '';
     return loggedUser;
   }
@@ -36,13 +33,9 @@ class AuthService {
     localStorage.setItem('id_token', idToken);
     window.location.assign('/');
   }
-    // TODO: set the token to localStorage
-    // TODO: redirect to the home page
 
   logout() {
-    // TODO: remove the token from localStorage
     localStorage.removeItem('id_token');
-    // TODO: redirect to the login page
     window.location.assign('/');
   }
 }
